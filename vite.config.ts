7bf7f6e966c8fa45e81c13a406256a32196c82f2
jiazhigang <jiazhigang_888@163.com>
2024-04-09 11:29:06 +0800

    feat: 新增项目命名

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
+// https://vitejs.dev/config/
 import { defineConfig, loadEnv, ConfigEnv, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-// https://vitejs.dev/config/
 
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     const root = process.cwd();
     const env = loadEnv(mode, root);
-    console.log(env);
 
     return {
         base: env.VITE_PUBLIC_PATH,
