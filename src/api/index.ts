b517c87a2043864f13f20718d8c1849b758d914f
jiazhigang <jiazhigang_888@163.com>
2024-04-12 10:53:22 +0800

    feat: axiosCancel文件添加

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import type { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import type { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import useEnv from '@/hooks/useEnv';
 import { ResultEnum } from '@/enums/httpEnum';
+import { CustomAxiosRequestConfig, ResultData } from './interface';
 
 const { VITE_BASE_API } = useEnv();
 const serviceConfig = {
@@ -22,7 +23,7 @@ class HttpRequest {
          * token校验(JWT) : 接受服务器返回的 token,存储到 vuex/pinia/本地储存当中
          */
         this.service.interceptors.request.use(
-            (config: InternalAxiosRequestConfig) => {
+            (config: CustomAxiosRequestConfig) => {
                 return config;
             },
             (error: AxiosError) => {
@@ -35,7 +36,7 @@ class HttpRequest {
          *  服务器换返回信息 -> [拦截统一处理] -> 客户端JS获取到信息
          */
         this.service.interceptors.response.use(
-            (response: AxiosResponse) => {
+            (response: AxiosResponse & { config: CustomAxiosRequestConfig }) => {
                 const { data } = response;
                 // 成功请求（在页面上除非特殊情况，否则不用处理失败逻辑）
                 return data;
@@ -44,6 +45,25 @@ class HttpRequest {
                 return Promise.reject(error);
             }
         );
+    }
+
+    /**
+     * @description 常用请求方法封装
+     */
+    get<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+        return this.service.get(url, { params, ..._object });
+    }
+    post<T>(url: string, params?: object | string, _object = {}): Promise<ResultData<T>> {
+        return this.service.post(url, params, _object);
+    }
+    put<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+        return this.service.put(url, params, _object);
+    }
+    delete<T>(url: string, params?: any, _object = {}): Promise<ResultData<T>> {
+        return this.service.delete(url, { params, ..._object });
+    }
+    download(url: string, params?: object, _object = {}): Promise<BlobPart> {
+        return this.service.post(url, params, { ..._object, responseType: 'blob' });
     }
 }
 export default new HttpRequest(serviceConfig);
