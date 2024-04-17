8b36cf15875752c0f21617dbf5d33cbe254bbc5e
jiazhigang <jiazhigang_888@163.com>
2024-04-17 13:45:02 +0800

    feat: 增加vueJsx

--- build/plugins.ts
+++ build/plugins.ts
@@ -2,11 +2,13 @@ import { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import vueSetupExtend from 'unplugin-vue-setup-extend-plus/vite';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export const vitePlugins = (viteEnv: ViteEnv): PluginOption[] => {
     const { VITE_GLOB_APP_TITLE } = viteEnv;
     return [
         vue(),
+        vueJsx(),
         // 注入变量到 html 文件
         createHtmlPlugin({
             minify: true,
