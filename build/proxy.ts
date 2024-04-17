80ffdf82bb480a83b2ab5e147f0603914972dc7d
jiazhigang <jiazhigang_888@163.com>
2024-04-17 14:28:05 +0800

    update: 增加注释

--- build/proxy.ts
+++ build/proxy.ts
@@ -4,7 +4,7 @@ type ProxyItem = [string, string];
 type ProxyList = ProxyItem[];
 type ProxyTargetList = Record<string, ProxyOptions>;
 
-export const creatProxy = (list: ProxyList) => {
+export const creatProxy = (list: ProxyList): ProxyTargetList => {
     const ret: ProxyTargetList = {};
     for (const [prefix, target] of list) {
         const httpsRE = /^https:\/\//;
@@ -12,7 +12,7 @@ export const creatProxy = (list: ProxyList) => {
         ret[prefix] = {
             target,
             changeOrigin: true,
-            // 重写
+            // 重写 --- 替换前缀
             rewrite: path => path.replace(new RegExp(`^${prefix}`), ''),
             // https 需要设置 secure=false
             ...(isHttps ? { secure: false } : {})
