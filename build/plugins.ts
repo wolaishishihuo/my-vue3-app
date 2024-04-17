3d60f85a30e01aa0496552544ef8ddfa323e02d6
jiazhigang <jiazhigang_888@163.com>
2024-04-17 14:09:46 +0800

    feat: 增加gzip压缩

--- build/plugins.ts
+++ build/plugins.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import vueSetupExtend from 'unplugin-vue-setup-extend-plus/vite';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import viteCompression from 'vite-plugin-compression';
 
 export const vitePlugins = (viteEnv: ViteEnv): PluginOption[] => {
     const { VITE_GLOB_APP_TITLE } = viteEnv;
@@ -17,6 +18,23 @@ export const vitePlugins = (viteEnv: ViteEnv): PluginOption[] => {
             }
         }),
         // script setup 上添加name属性
-        vueSetupExtend({})
+        vueSetupExtend({}),
+        creatCompression(viteEnv)
     ];
+};
+
+const creatCompression = (viteEnv: ViteEnv): PluginOption | PluginOption[] => {
+    const { VITE_BUILD_COMPRESS } = viteEnv;
+    const compressList = VITE_BUILD_COMPRESS.split(',');
+    const plugins: PluginOption[] = [];
+    if (compressList.includes('gzip')) {
+        plugins.push(
+            viteCompression({
+                algorithm: 'gzip',
+                ext: '.gz',
+                deleteOriginFile: true
+            })
+        );
+    }
+    return plugins;
 };
